07842d24a8653d880285f1e5c92c61a3937d6121
Jackson Souza Santos <jacksonsantos.tec@gmail.com>
2023-02-21 16:14:30 -0300

    refatorando chamada para API da pagina search

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,102 +1,45 @@
-// import React from 'react';
-// import propTypes from 'prop-types';
-// import Header from '../components/Header';
-// import getMusics from '../services/musicsAPI';
-// import MusicCard from '../components/MusicCard';
-
-// class Album extends React.Component {
-//   constructor() {
-//     super();
-
-//     this.state = {
-//       artistName: '',
-//       albumName: '',
-//       musics: [],
-//     };
-//   }
-
-//   componentDidMount() {
-//     this.musicData();
-//   }
-
-//   musicData = async () => {
-//     const { match: { params: { id } } } = this.props;
-//     const album = await getMusics(id);
-
-//     this.setState({
-//       artistName: album[0].artistName,
-//       albumName: album[0].collectionName,
-//       musics: [...album],
-//     });
-//   };
-
-//   render() {
-//     const { artistName, albumName, musics } = this.state;
-//     return (
-//       <div data-testid="page-album">
-//         <Header />
-//         <h1 data-testid="artist-name">{ artistName }</h1>
-//         <h2 data-testid="album-name">{ albumName }</h2>
-//         <MusicCard musics={ musics } />
-//       </div>
-//     );
-//   }
-// }
-
-// Album.propTypes = {
-//   match: propTypes.shape({
-//     params: propTypes.shape({
-//       id: propTypes.string.isRequired,
-//     }).isRequired,
-//   }).isRequired,
-// };
-
-// export default Album;
-
 import React from 'react';
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
 import MusicCard from '../components/MusicCard';
+import '../styles/Album.css';
 
 class Album extends React.Component {
   constructor() {
     super();
+
     this.state = {
-      artistMusics: [],
-      albumData: '',
+      artistName: '',
+      albumName: '',
+      musics: [],
     };
   }
 
   componentDidMount() {
-    this.fetchMusics();
+    this.musicData();
   }
 
-  fetchMusics = async () => {
-    const { match } = this.props;
-    const [albumData, ...artistMusics] = await getMusics(match.params.id);
+  musicData = async () => {
+    const { match: { params: { id } } } = this.props;
+    const album = await getMusics(id);
+
     this.setState({
-      artistMusics: [...artistMusics],
-      albumData,
+      artistName: album[0].artistName,
+      albumName: album[0].collectionName,
+      musics: [...album],
     });
   };
 
   render() {
-    const { artistMusics, albumData } = this.state;
+    const { artistName, albumName, musics } = this.state;
     return (
       <div data-testid="page-album" className="page-album">
         <Header />
         <div className="songs-container">
-          <div className="page-title">
-            <h2 data-testid="artist-name">{albumData.artistName}</h2>
-            <h3 data-testid="album-name">{albumData.collectionName}</h3>
-          </div>
-          {artistMusics.map((music) => (
-            <MusicCard
-              key={ music.trackId }
-              musicData={ music }
-            />
-          ))}
+          <h1 data-testid="artist-name">{ artistName }</h1>
+          <h2 data-testid="album-name">{ albumName }</h2>
+          <MusicCard musics={ musics } />
         </div>
       </div>
     );
@@ -104,9 +47,9 @@ class Album extends React.Component {
 }
 
 Album.propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      id: PropTypes.string.isRequired,
+  match: propTypes.shape({
+    params: propTypes.shape({
+      id: propTypes.string.isRequired,
     }).isRequired,
   }).isRequired,
 };
